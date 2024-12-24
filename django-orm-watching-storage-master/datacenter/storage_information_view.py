a9d12dfaaae872dd858749766d648751b449b569
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-12-24 16:30:57 +0300

    fixed output in `storage_information_view.py`

--- django-orm-watching-storage-master/datacenter/storage_information_view.py
+++ django-orm-watching-storage-master/datacenter/storage_information_view.py
@@ -5,19 +5,23 @@ from datacenter.visit_utils import get_duration, format_duration
 
 def storage_information_view(request):
     active_visitors = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+
     for visitor in active_visitors:
         name = visitor.passcard
         duration, entered_at = get_duration(visitor)
         duration = format_duration(duration)
 
-    non_closed_visits = [
-        {
+        visitor_dict = {
             'who_entered': name,
             'entered_at': entered_at,
             'duration': duration,
         }
-    ]
+
+        non_closed_visits.append(visitor_dict)
+
     context = {
         'non_closed_visits': non_closed_visits,
     }
+    
     return render(request, 'storage_information.html', context)
